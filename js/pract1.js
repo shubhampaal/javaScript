b0f957ae5b21d320d3b3dac63c5c9cd9299b11b2
shubhampaal <shivampal7248841916@gmail.com>
2024-07-16 11:03:33 +0530

    loops

---
+++ js/pract1.js
@@ -0,0 +1,25 @@
+// let a="shubhampal";
+// let b="hello";
+//defference btw strring and variable 
+// console.log(a ,"pal")
+// let n=10;
+// while(n<21){
+//     console.log(n)
+//     n++
+// }
+
+
+// let n=1;
+// while(n<=10){
+//     console.log(n*5)
+//     n++
+// }
+for (let i=1; i<=20; i++){
+if(i%2==0){
+    console.log("no is even " ,i )
+
+}
+else{
+    console.log("no is odd" ,i )
+}
+}
